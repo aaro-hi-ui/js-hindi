d9f46332516b5e590c20a95465dc49df373dd887
aaro-hi-ui <130505638+aaro-hi-ui@users.noreply.github.com>
2024-11-23 10:05:27 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //use all js code as newer version 
+
+// alert( 3 + 3) we are using node.js,not browser
+
+console.log(  3 + 3); // code readiability should be high
+
+console.log("Aarohi");
+
+
+let name="AAROHI"
+let age = 19
+let isloggedin= false
+let state = null
+
+
+
+// number =>2 power 53
+// bigInt
+// string =>""
+// Boolean =>true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+ 
+
+console.log(typeof null);
+
+console.log(typeof undefined);
