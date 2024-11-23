8e2128432f39719233b0a215e8b0a735e7bf9b1e
aaro-hi-ui <130505638+aaro-hi-ui@users.noreply.github.com>
2024-11-23 09:47:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=1134335
+let accountEmail="aarohigangwar@gmail.com"
+let accountPassword="281103"
+accountCity="Dehli"
+let accountNumber;
+
+//accountId=2 // not allowed
+
+accountEmail="aaro@gmail.com"
+accountPassword="122342"
+accountCity="Bareilly"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountNumber]);
+/*
+Pefer not use use var
+because of issue in block scope and functional scope
+*/
