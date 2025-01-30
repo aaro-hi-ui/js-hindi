6bb42ae8a5abbe402aa458e1c1484e4c8b797a67
Aarohi Gangwar <130505638+aaro-hi-ui@users.noreply.github.com>
2025-01-30 12:14:06 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionofdatatypes.js
@@ -0,0 +1,29 @@
+let score =33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let state = "aarohi"
+ console.log(typeof state );
+ 
+ let valueInNumber =Number(state)
+console.log( typeof state);
+
+// 33 -->number
+// 33abc--> NaN
+//true-->1 false-->0
+
+let isLoggedIn = "aarohi"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn );
+ 
+//true-->1 false-->0
+// aarohi --> true
+// "" -->true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof someNumber);
+console.log(someNumber);
+
