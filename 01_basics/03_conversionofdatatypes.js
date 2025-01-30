8fb68635cd3fd08f3463948e2d5331a39363b135
Aarohi Gangwar <130505638+aaro-hi-ui@users.noreply.github.com>
2025-01-30 12:33:58 +0000

    conversion of datatypes

--- 01_basics/03_conversionofdatatypes.js
+++ 01_basics/03_conversionofdatatypes.js
@@ -27,3 +27,24 @@ let stringNumber = String(someNumber)
 console.log(typeof someNumber);
 console.log(someNumber);
 
+// ***********************************operations***************************************************
+ 
+console.log(3+3);
+ 
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(3**3);
+console.log(3%3);
+
+let workshop = 100
+++workshop
+console.log(workshop);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2"+2);
+console.log(+true);
+console.log(+"");
+
+
